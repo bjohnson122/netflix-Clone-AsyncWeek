5bf3edb5273eab378f94b584c2277d73a7024992
Breana Johnson <brecjohnson@gmail.com>
2022-10-20 23:35:54 -0400

    updated code to use 'react-router-dom'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import HomeScreen from "./HomeScreen";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
