de8f1dc1a6850bd2db3118abc71e89b2d618cfaf
Breana Johnson <brecjohnson@gmail.com>
2022-10-21 06:37:42 -0400

    login and state persistence with Redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomeScreen from "./HomeScreen";
 import "./App.css";
 import LoginScreen from "./LoginScreen";
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, login, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //LoggedIn
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        //Logged Out
+        dispatch(logout);
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
   return (
     <div className="app">
       <Router>
-      {!user ? (
-      <LoginScreen />
-      ) : (
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
         )}
       </Router>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,20 @@
-import firebase from 'firebase'
+// import firebase from "firebase";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDMwo12wyS5zU4hc0z2dQrOjpRUs3qDPzc",
-    authDomain: "netflix-clone-breanaj.firebaseapp.com",
-    projectId: "netflix-clone-breanaj",
-    storageBucket: "netflix-clone-breanaj.appspot.com",
-    messagingSenderId: "166322458313",
-    appId: "1:166322458313:web:9d94b91f6d0f8227b9c718"
-  };
+  apiKey: "AIzaSyDMwo12wyS5zU4hc0z2dQrOjpRUs3qDPzc",
+  authDomain: "netflix-clone-breanaj.firebaseapp.com",
+  projectId: "netflix-clone-breanaj",
+  storageBucket: "netflix-clone-breanaj.appspot.com",
+  messagingSenderId: "166322458313",
+  appId: "1:166322458313:web:9d94b91f6d0f8227b9c718",
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig)
-  const db = firebaseApp.firestore()
-  const auth = firebase.auth()
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
