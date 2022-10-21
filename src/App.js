3070b1b8788b8fff1313892637bd4638f1ec8c33
Breana Johnson <brecjohnson@gmail.com>
2022-10-21 02:35:08 -0400

    sign in component & welcome screen components complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import HomeScreen from "./HomeScreen";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoginScreen from "./LoginScreen";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 
 function App() {
+  const user = null
   return (
     <div className="app">
       <Router>
+      {!user ? (
+      <LoginScreen />
+      ) : (
         <Routes>
           <Route path="/" element={<HomeScreen />} />
         </Routes>
+        )}
       </Router>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyDMwo12wyS5zU4hc0z2dQrOjpRUs3qDPzc",
     authDomain: "netflix-clone-breanaj.firebaseapp.com",
@@ -6,3 +8,7 @@ const firebaseConfig = {
     messagingSenderId: "166322458313",
     appId: "1:166322458313:web:9d94b91f6d0f8227b9c718"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
+  const db = firebaseApp.firestore()
+  const auth = firebase.auth()
